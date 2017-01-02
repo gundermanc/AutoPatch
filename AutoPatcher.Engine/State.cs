750115660f947dc19001969b96edaa68291345ea
Christian Gunderman <gundermanc@gmail.com>
2017-01-01 18:48:41 -0800

    Removed extra using.

--- AutoPatcher.Engine/State.cs
+++ AutoPatcher.Engine/State.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 using AutoPatcher.Engine.Abstractions;
 using AutoPatcher.Engine.Properties;
